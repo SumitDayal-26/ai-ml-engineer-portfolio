769123c6a1f2afe179a43270ce6f3f971be30079
SumitDayal-26 <sumitdayal2006@gmail.com>
2026-02-09 11:57:33 +0530

    Add Day 1 Python basics

---
+++ 01_python_basics/day01_basics.py
@@ -0,0 +1,26 @@
+# Day 1: python basics 
+# 1. Printing output
+print("hello Engineer")
+
+# 2. Variables and data types
+name = "Sumit"
+age = 20
+cgpa = 8.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Cgpa:", cgpa)
+print("Student:", is_student)
+
+# 3. Taking input from users 
+city = input("enter your City:")
+print("City:", city)
+
+# 4. Simple calculations
+a = int(input("enter value of a:"))
+b = int(input("enter value of b:"))
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+
